a904602c4d1e34d02c41841a7a919cb93fcbfed4
Andre Ambrosio Boechat <boechat107@gmail.com>
2019-02-17 16:04:31 -0300

    Add solution to exercise 1.8

--- lecture_1a.rkt
+++ lecture_1a.rkt
@@ -26,15 +26,31 @@
   (try 1.0 x))
 
 
+(define (good-enough?* guess-new guess-old)
+  (< (/ (abs (- guess-new guess-old))
+        guess-new)
+     0.001))
+
+;; Exercise 1.7
 ;; Definition using a better strategy for "good-enough?".
 (define (sqrt* x)
-  (define (good-enough? guess-new guess-old)
-    (< (/ (abs (- guess-new guess-old))
-          guess-new)
-       0.001))
   (define (try guess-new guess-old)
-    (if (good-enough? guess-new guess-old)
+    (if (good-enough?* guess-new guess-old)
         guess-new
         (try (average guess-new (/ x guess-new))
+             guess-new)))
+  (try 1.0 0.0))
+
+
+;; Exercise 1.8
+(define (cube-root x)
+  (define (approx guess)
+    (/ (+ (/ x (square guess))
+          (* 2 guess))
+       3))
+  (define (try guess-new guess-old)
+    (if (good-enough?* guess-new guess-old)
+        guess-new
+        (try (approx guess-new)
              guess-new)))
   (try 1.0 0.0))
